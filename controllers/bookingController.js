3cdd0ca1bec0462f925c143b4a6cd73a2fc5eddb
Meer Uzair <meer.uxair007@gmail.com>
2024-02-04 14:44:34 +0530

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,29 +1,45 @@
-import { formatImage } from "../middleware/multerMiddleware.js";
-import cloudinary from "cloudinary";
-import { BadRequestError } from "../errors/customErrors.js";
 import { StatusCodes } from "http-status-codes";
-
+import Booking from '../models/Booking.js'
 
 
 export const createBooking = async (req,res)=>{
-    res.status(StatusCodes.OK).json({ msg:"createBooking" });
+
+  const booking = await Booking.create(req.body);
+
+  res.status(StatusCodes.OK).json({ booking });
 }
 
 export const getAllBookings = async (req,res)=>{
-  res.status(StatusCodes.OK).json({ msg:"getAllBookings" });
+  const bookings = await Booking.find({userId:req.user.userId})
+  res.status(StatusCodes.OK).json({ bookings });
+}
+
+export const getAllBookingsForAdmin = async (req,res)=>{
+  const bookings = await Booking.find({}).populate({path:'userId', select:"name email phoneNumber"}).populate({path:'packageId', select:"packageTitle startingPrice"})
+  const totalBookings = await Booking.countDocuments();
+  res.status(StatusCodes.OK).json({ totalBookings ,bookings, });
 }
 
 export const getSingleBooking = async (req,res)=>{
 const {id} = req.params;
-res.status(StatusCodes.OK).json({ msg:"getSingleBooking" });
+const booking = await Booking.findOne({_id:id})
+res.status(StatusCodes.OK).json({ booking });
 }
 
 export const updateBooking  = async (req,res)=>{
 const {id} = req.params;
+  await Booking.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+ 
   res.status(StatusCodes.OK).json({ msg:"Booking Updated Successfully"});
 }
 
 export const deleteBooking = async (req,res)=>{
 const {id} = req.params;
+await Booking.findOneAndDelete({_id:id})
+
   res.status(StatusCodes.OK).json({ msg:"Booking Deleted Successfully" });
 }
