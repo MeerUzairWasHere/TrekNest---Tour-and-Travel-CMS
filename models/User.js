9a6eb28954d7b4d2d767ec96f6084c4b4838acba
MeerUzairWasHere <meer.uxair007@gmail.com>
2024-04-26 19:46:59 +0530

    set default user isVerified to true as email service is not working somehow

--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
   verificationToken: String,
   isVerified: {
     type: Boolean,
-    default: false,
+    default: true,
   },
   verified: Date,
   passwordToken: {
